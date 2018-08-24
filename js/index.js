5da8910daf70fb5110b225c6cd28bd1b1ce35cb3
xiaoqiang520520 <1120389548@qq.com>
2018-08-24 11:48:10 +0800

    3

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@ window.onload=function () {
   let bannerimgs=document.querySelectorAll(".banner-img li");
   let switchover=document.querySelector(".switchover");
     let switchoverr=document.querySelector(".switchoverr");
-    let roundframes=document.querySelectorAll(".round-frame");
-    console.log(bannerimgs,switchover,switchoverr);
+    let roundframes=document.querySelectorAll(".Round-frame ul li");
+    let flag=true;
+
+    console.log(bannerimgs,switchover,switchoverr,roundframes);
 
 
-  let t1=setInterval(mover,2000);
+  // let t1=setInterval(mover,2000);
   //   function move(){
   //
   //   }
@@ -20,25 +22,50 @@ window.onload=function () {
 
     function mover() {
 
+
       next++;
       if(next==bannerimgs.length){
         next=0;
       }
         bannerimgs[next].style.left="1226px";
         animate(bannerimgs[now],{left:-1226});
-        animate(bannerimgs[next],{left:0});
+        animate(bannerimgs[next],{left:0},function () {
+            flag=true;
+
+        });
       // bannerimgs[now].left="0";
       // bannerimgs[next].left="1226px";
+        roundframes[now].classList.remove("hot");
+        roundframes[next].classList.add("hot");
+
         now=next;
 
     }
-    switchoverr.onclick=mover;
-    switchover.onclick=function movel() {
-      clearInterval(t1);
-        switchover.onmouseleave=function () {
-            t1=setInterval(mover,2000);
-
+    switchoverr.onclick=function () {
+        if(flag==false){
+            return;
         }
+        if(next==bannerimgs.length-1){
+            return;
+        }
+        flag=false;
+        mover();
+
+    };
+    switchover.onclick=function movel() {
+        if(flag==false){
+            return;
+        }
+        if(next==0){
+            return;
+        }
+        flag=false;
+      // clearInterval(t1);
+      //   switchover.onmouseleave=function () {
+      //       t1=setInterval(mover,2000);
+      //
+      //   }
+
 
         next--;
         if(next<0){
@@ -46,37 +73,57 @@ window.onload=function () {
         }
         bannerimgs[next].style.left="-1226px";
         animate(bannerimgs[now],{left:1226});
-        animate(bannerimgs[next],{left:0});
+        animate(bannerimgs[next],{left:0},function () {
+            flag=true;
+
+        });
+        roundframes[now].classList.remove("hot");
+        roundframes[next].classList.add("hot");
         // bannerimgs[now].left="0";
         // bannerimgs[next].left="1226px";
         now=next;
 
+
     }
 
 
-    bannerimgs.forEach(function (v,i) {
-        v.onmouseenter=function () {
-            clearInterval(t1);
-
-        }
-        v.onmouseleave=function () {
-            t1=setInterval(mover,2000);
-
-        }
-
-    })
+    // bannerimgs.forEach(function (v,i) {
+    //     v.onmouseenter=function () {
+    //         clearInterval(t1);
+    //
+    //     }
+    //     v.onmouseleave=function () {
+    //         t1=setInterval(mover,2000);
+    //
+    //     }
+    //
+    // })
 
     roundframes.forEach(function (v,i) {
         v.onclick=function () {
-            v.style.background="#fff";
-            animate(bannerimgs[i],{left:0});
+            // v.style.background="#fff";
+            // animate(bannerimgs[i],{left:0});
+
+            if(now==i){
+                return;
+            }else if(now<i){
+                bannerimgs[i].style.left="1226px";
+                animate(bannerimgs[now],{left:-1226});
+                animate(bannerimgs[i],{left:0});
+                roundframes[now].classList.remove("hot");
+                roundframes[i].classList.add("hot");
+
+
+            }else{
+                bannerimgs[i].style.left="-1226px";
+                animate(bannerimgs[now],{left:1226});
+                animate(bannerimgs[i],{left:0});
+                roundframes[now].classList.remove("hot");
+                roundframes[i].classList.add("hot");
+
+            }
+            now=next=i;
         }
-        // if(i>now){
-        //     mover();
-        //
-        // }else{
-        //     movel();
-        // }
 
     })
 
